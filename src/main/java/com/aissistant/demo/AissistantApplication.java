db136291877e93e6f5ef46582966a867a63b883d
VladGaranovskyi <garanovskiyvlad2005@gmail.com>
2025-07-20 14:55:08 -0400

    1.0

---
+++ src/main/java/com/aissistant/demo/AissistantApplication.java
@@ -0,0 +1,13 @@
+package com.aissistant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AissistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AissistantApplication.class, args);
+	}
+
+}
